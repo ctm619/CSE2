93c5a40c3e5d037d6127daf5713a47f794292eeb
ctm619 <ctm619@lehigh.edu>
2015-08-27 15:31:13 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+//Caleb Mandile
+//Hello World java program
+//define the class
+public class HelloWorld {
+    
+//Add main method
+    public static void main (String []args){
+        
+        System.out.println("#############################");
+        System.out.println("#############################");
+        System.out.println("##                         ##");
+        System.out.println("##   Hello World 2015!!!   ##");
+        System.out.println("##                         ##");
+        System.out.println("#############################");
+        System.out.println("#############################");
+        System.out.println("test");
+    }
+}
