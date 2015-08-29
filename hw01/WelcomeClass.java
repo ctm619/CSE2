09e9d1b1c90bb230ef3110abb1444bb345072bde
ctm619 <ctm619@lehigh.edu>
2015-08-29 23:59:20 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,14 @@
+public class WelcomeClass {
+    public static void main (String []args) {
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-C--T--M--6--1--9-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("Last summer my family moved from Scranton, PA");
+        System.out.println("to Marathon, Fl. I live about 100 miles from Cuba.");
+    }
+}
